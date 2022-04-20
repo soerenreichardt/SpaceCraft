86f9baf8f9a76678cde2b6ecd15bfca4b183b5fa
Sören Reichardt <soren.reichardt@neotechnology.com>
2022-04-20 21:51:44 +0200

    Refactor TerrainQuadTree

--- Scripts/Terrain/TerrainQuadTree.cs
+++ Scripts/Terrain/TerrainQuadTree.cs
@@ -1,8 +1,6 @@
-using System;
 using DataStructures;
 using Terrain;
 using UnityEngine;
-using static DataStructures.Directions;
 
 public class TerrainQuadTree : AdaptiveSpatialQuadTree<TerrainQuadTree>
 {
@@ -70,12 +68,13 @@ public class TerrainQuadTree : AdaptiveSpatialQuadTree<TerrainQuadTree>
 
     protected override TerrainQuadTree initialize(int quadrant)
     {
-        Vector3 newCenter = computeCenter(quadrant);
         var nextLevel = level + 1;
+        var newChunkLength = chunkLength / 2;
+        var newCenter = TerrainQuadTreeHelper.computeCenter(face, center, quadrant, newChunkLength);
         return new TerrainQuadTree(
             newCenter, 
             planetPosition, 
-            chunkLength / 2, 
+            newChunkLength, 
             face, 
             viewDistance, 
             maxLevel, 
@@ -95,60 +94,11 @@ public class TerrainQuadTree : AdaptiveSpatialQuadTree<TerrainQuadTree>
         // TODO: consider Vector3.SqrtMagnitude to avoid sqrt computation
         return Mathf.Abs(Vector3.Distance(camera.transform.position, terrainComponent.vertices[8 * (MeshGenerator.CHUNK_SIZE+1) + 8] + terrainComponent.transform.position));
     }
-    
-    private Vector2 compute2DCenter(float anchorX, float anchorY, int quadrant, float sign)
-    {
-        Vector2 result;
-        float halfLength = chunkLength / 2;
-        switch (quadrant)
-        {
-            case TOP_LEFT:
-                result = new Vector2(anchorX - (halfLength * sign), anchorY + halfLength);
-                break;
-            case TOP_RIGHT:
-                result = new Vector2(anchorX + (halfLength * sign), anchorY + halfLength);
-                break;
-            case BOTTOM_LEFT:
-                result = new Vector2(anchorX - (halfLength * sign), anchorY - halfLength);
-                break;
-            case BOTTOM_RIGHT:
-                result = new Vector2(anchorX + (halfLength * sign), anchorY - halfLength);
-                break;
-            default:
-                throw new ArgumentException("Parameter quadrant should be within 0 and 3");
-        }
-        return result;
-    }
-
-    // TODO: refactor to use axis
-    private Vector3 computeCenter(int quadrant)
-    {
-        Vector3 result;
-        Vector2 centerOnPlane;
-        if (face == Vector3.up || face == Vector3.down)
-        {
-            centerOnPlane = compute2DCenter(center.x, center.z, quadrant, face.y);
-            result = new Vector3(centerOnPlane.x, center.y, centerOnPlane.y);
-        } 
-        else if (face == Vector3.left || face == Vector3.right)
-        {
-            centerOnPlane = compute2DCenter(center.z, center.y, quadrant, face.x);
-            result = new Vector3(center.x, centerOnPlane.y, centerOnPlane.x);
-        }
-        else if (face == Vector3.forward || face == Vector3.back)
-        {
-            centerOnPlane = compute2DCenter(center.x, center.y, quadrant, -face.z);
-            result = new Vector3(centerOnPlane.x, centerOnPlane.y, center.z);
-        }
-        else throw new ArgumentException("Parameter face should be one of the 6 following directions: [left, right, up, down, forward, back]");
-        return result;
-    }
 
     protected override void adaptiveTreeOnSplit()
     {
         foreach (TerrainQuadTree child in children) {
-            MeshGenerator.Data childData = new MeshGenerator.Data(child.terrainComponent, child.center, child.face, child.chunkLength, level == maxLevel - 1);
-            MeshGenerator.pushData(childData);
+            child.computeTerrain();
             child.computeNeighbors();
         }
     }
@@ -172,6 +122,12 @@ public class TerrainQuadTree : AdaptiveSpatialQuadTree<TerrainQuadTree>
         updateMeshIndices();
     }
 
+    private void computeTerrain()
+    {
+        MeshGenerator.Data childData = new MeshGenerator.Data(terrainComponent, center, face, chunkLength, level == maxLevel);
+        MeshGenerator.pushData(childData);
+    }
+    
     private void updateMeshIndices()
     {
         if (!isBlockLevel())
---
+++ Scripts/Terrain/TerrainQuadTreeHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using static DataStructures.Directions;
+
+namespace Terrain
+{
+    public class TerrainQuadTreeHelper
+    {
+        // TODO: refactor to use axis
+        internal static Vector3 computeCenter(Vector3 face, Vector3 center, int quadrant, float chunkLength)
+        {
+            Vector3 result;
+            Vector2 centerOnPlane;
+            if (face == Vector3.up || face == Vector3.down)
+            {
+                centerOnPlane = compute2DCenter(center.x, center.z, quadrant, face.y, chunkLength);
+                result = new Vector3(centerOnPlane.x, center.y, centerOnPlane.y);
+            }
+            else if (face == Vector3.left || face == Vector3.right)
+            {
+                centerOnPlane = compute2DCenter(center.z, center.y, quadrant, face.x, chunkLength);
+                result = new Vector3(center.x, centerOnPlane.y, centerOnPlane.x);
+            }
+            else if (face == Vector3.forward || face == Vector3.back)
+            {
+                centerOnPlane = compute2DCenter(center.x, center.y, quadrant, -face.z, chunkLength);
+                result = new Vector3(centerOnPlane.x, centerOnPlane.y, center.z);
+            }
+            else
+                throw new ArgumentException(
+                    "Parameter face should be one of the 6 following directions: [left, right, up, down, forward, back]");
+
+            return result;
+        }
+
+        private static Vector2 compute2DCenter(float anchorX, float anchorY, int quadrant, float sign, float chunkLength)
+        {
+            Vector2 result;
+            switch (quadrant)
+            {
+                case TOP_LEFT:
+                    result = new Vector2(anchorX - (chunkLength * sign), anchorY + chunkLength);
+                    break;
+                case TOP_RIGHT:
+                    result = new Vector2(anchorX + (chunkLength * sign), anchorY + chunkLength);
+                    break;
+                case BOTTOM_LEFT:
+                    result = new Vector2(anchorX - (chunkLength * sign), anchorY - chunkLength);
+                    break;
+                case BOTTOM_RIGHT:
+                    result = new Vector2(anchorX + (chunkLength * sign), anchorY - chunkLength);
+                    break;
+                default:
+                    throw new ArgumentException("Parameter quadrant should be within 0 and 3");
+            }
+            return result;
+        }
+    }
+}
